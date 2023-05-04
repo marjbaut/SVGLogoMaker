953069d6b2f3d25b70b6c5fbb164ad214abd21e5
Marjorie <marjoriebaut.w@gmail.com>
2023-05-03 23:51:24 -0400

    done

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # SVGLogoMaker
+
+## Table of Content
+-[project description](#Description)
+-[usage](#Usage)
+-[contributing](#Contributing)
+-[installation](#Installation)
+-[Questions](#Questions)
+-[License](#License)
+
+## Description
+
+The application prompts the user to enter the text, select the shape and enter a color. Once propmt are completed, a svg file will generated. This application is useful for freelance web developers to generate simple logos for their projects, so that a professional graphic designer is not required.
+
+
+## Usage
+
+the user will have to answer the promps in other to generate a logo
+## Installation
+
+You will first need to install the following: 
+    fs: 0.0.1-security,
+    inquirer: ^8.2.4,
+    jest: ^29.5.0,
+    path: ^0.12.7
--- index.js
+++ index.js
@@ -1,38 +1,17 @@
-
 const inquirer = require("inquirer");
 const fs = require('fs');
+const path = require('path');
 // const {Circle, Triangle, square} = require ('./lib/shapes')
-const {Shape, Circle, Square, Triangle} = require('./lib/shapes')
-
-// // class Svg{
-// //     constructor(){
-// //         this.textElement = ''
-// //         this.shaperElement =''
-// //     }
-// //     render(){
-// //         return `<svg version="1.1" width="300" height="200" xmlns='http://www.w3.org/2000/svg'>
-// //         <rect x='90' y='40' width='120' height='120' fill="black"/>
-// //         </svg> `
-// //     }
-// //     setText(text,textColor){
-// //         this.textElement=` <text x='150' y='125' fonst-size='60' text-anchor='middle' fill='${textColor}'>${text}
-// //         </text>`
-// //     }
-// //     setShapeElement(shape){
-// //         this.textElement=shape.render()
-// //     }
-// // }
-
-// // {/* <svg version="1.1" width="300" height="200" xmlns='http://www.w3.org/2000/svg'><rect x='90' y='40' width='120' height='120' fill="black"/><text x='150' y='125' fonst-size='60' text-anchor='middle' fill='orange'>text</text></svg> */}
-
+const {Circle, Square, Triangle }= require('./lib/shapes')
+const svggenerator= require('./lib/svggenerator');
 
 const questions = [
     {type:'input',
-    name:'logoTxt',
+    name:'text',
     message:'Enter up to three characters to display in yor logo: '
     },
     {type:'input',
-    name:'textColor',
+    name:'color',
     message:'Enter text color keyword (OR a hexadecimal number): ' 
     },
     {type:'input',
@@ -40,31 +19,54 @@ const questions = [
     message:'Enter shape color keyword (OR a hexadecimal number): '
     },
     {
-        type:'list',
-        name:'logoShape',
-        message:'Pick a shape for your logo: ',
-        choices:['circle', 'triangle','square'],
-        filter(val){
-            return val.toLowerCase();
-        }
+    type:'list',
+    name:'shape',
+    message:'Pick a shape for your logo: ',
+    choices:['circle', 'triangle','square'],
     },
     ];
-
+let shape;
 function askQuestion(){
     return inquirer.prompt(questions)
         .then( (answers)=>{
-            const shapeInput = Shape.generateLogo(answers)
-            fs.writeFile('logo.svg',shapeInput, function(err){
-                if (err){
-                    console.log('could not save',err)
-                } else{
-                    console,log('success')
-                }
-            }) 
-        }
-        )
-        .catch((error)=>{
-            console.log(error)
+            switch(answers.shape){
+                case 'circle':
+                    shape = new Circle();
+                    break;
+                case 'square':
+                    shape = new Square();
+                    break;
+                case 'triangle':
+                    shape = new Triangle();
+                    break;
+            }
+
+    const svg = new svggenerator.SVG();
+svg.setText(answers.text, answers.color);
+shape.setColor(answers.shapeColor);
+svg.setShape(shape);
+
+    const svgString = svg.render();
+    return writeToFile("logo.svg", svgString)        
         })
-}
+
+    function writeToFile(fileName, data) {
+        fs.writeFile (fileName, data, (err) => {
+            if (err)
+            return console.log(err);
+            else {
+                console.log("Success! Generated logo.svg");
+            }
+        });
+    }
+    writeToFile (svggenerator.SVG, svgString);
+
+    }
+
+
 askQuestion();
+
+
+   
+
+
